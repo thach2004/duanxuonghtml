0c43c5867fb3f48de574eaedfb6eeb9d84045b71
Nguyễn Hoàng Thạch <147287525+thach2004@users.noreply.github.com>
2024-03-21 23:10:30 +0700

    init

---
+++ DuAnXuong-SCSS-main/vite.config.js
@@ -0,0 +1,17 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+        product: resolve(__dirname, "./product.html"),
+        productdetail: resolve(__dirname, "./product-detail.html"),
+        cart: resolve(__dirname, "./cart.html"),
+        checkout: resolve(__dirname, "./checkout.html"),
+        login: resolve(__dirname, "./login.html"),
+      },
+    },
+  },
+});
